a7aeafa4b6f41cebd9501a385a30e724217ea150
xerocb <ciaransbrady@gmail.com>
2025-03-20 14:37:09 +0000

    Add user slice

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,67 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const login = createAsyncThunk("user/login", async (arg, thunkAPI) => {
+    const { username, password } = arg;
+    try {
+        const response = await attemptLogin(username, password);
+        if (!response.ok) {
+            return thunkAPI.rejectWithValue(`Error ${response.status}: Login failed`);
+        }
+        return await response.json();
+    } catch(err) {
+        return thunkAPI.rejectWithValue(err.message);
+    }
+});
+
+export const logout = createAsyncThunk("user/logout", async (arg, thunkAPI) => {
+    try {
+        const response = await attemptLogout();
+        if (!response.ok) {
+            return thunkAPI.rejectWithValue(`Error ${response.status}: Logout failed`);
+        }
+        return await response.json();
+    } catch(err) {
+        return thunkAPI.rejectWithValue(err.message);
+    }
+});
+
+const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        id: 0,
+        loading: false,
+        error: null
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(login.fulfilled, (state, action) => {
+                state.id = action.payload;
+            })
+            .addCase(logout.fulfilled, (state) => {
+                state.id = 0;
+            })
+            .addMatcher(
+                (action) => action.type.endsWith("/pending"),
+                (state) => {
+                    state.loading = true;
+                    state.error = null;
+                }
+            )
+            .addMatcher(
+                (action) => action.type.endsWith("/fulfilled"),
+                (state) => {
+                    state.loading = false;
+                    state.error = null;
+                }
+            )
+            .addMatcher(
+                (action) => action.type.endsWith("/rejected"),
+                (state, action) => {
+                    state.loading = false;
+                    state.error = action.payload;
+                }
+            )
+    }
+});
+
+export default userSlice.reducer;
